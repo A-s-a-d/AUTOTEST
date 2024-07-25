3e1e7391be63074b0e03ae275f43493e6409a8dc
A-s-a-d <81926998+A-s-a-d@users.noreply.github.com>
2024-07-25 11:15:40 +0200

    Added the fonctionnalities for 2 types of acutator using adafruit 2927 motor driver

--- lib/DEBUG/debug_timing_main.h
+++ lib/DEBUG/debug_timing_main.h
@@ -1,18 +1,18 @@
 #ifndef DEBUG_TIMING_MAIN_H
 #define DEBUG_TIMING_MAIN_H
 #include "debug_variable.h"
-
+#include <Arduino.h>
 
 inline void debugStartTime(unsigned long &startLoop_time)
 {
-    startLoop_time = micros();
+    startLoop_time = esp_timer_get_time();
 }
 
 inline void debugEndTime(unsigned long &startLoop_time)
 {
-    unsigned long endLoop = micros();
-    Serial.print("Loop ");
+    unsigned long endLoop = esp_timer_get_time();
     Serial.println(endLoop - startLoop_time);
+    Serial.print(" µs");
 }
 
 #endif
